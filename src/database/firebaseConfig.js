66885fb4ce71d41c5d0d65cb10906df6cd86bc7a
Yuliia Vorobei <vorobeju1@gmail.com>
2025-02-20 21:03:10 +0100

    fixed database info

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, push } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXlFA84x5vM_WY4b7LjEhCmE3Mstpq3dU",
-  authDomain: "learnlingo-18ec5.firebaseapp.com",
-  databaseURL:
-    "https://learnlingo-18ec5-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "learnlingo-18ec5",
-  storageBucket: "learnlingo-18ec5.firebasestorage.app",
-  messagingSenderId: "1006573973896",
-  appId: "1:1006573973896:web:217beee9c47a9cbc258a3d",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
