7ca19fb4d90da3a53b06cc01ab130088d9a25471
Yuliia Vorobei <vorobeju1@gmail.com>
2025-03-01 23:45:32 +0100

    edited pics

--- src/components/Memoji/Memoji.jsx
+++ src/components/Memoji/Memoji.jsx
@@ -1,17 +1,12 @@
 import css from "./Memoji.module.css";
+import memoji from "../../assets/memoji.png";
+import laptop from "../../assets/Mac-removebg-preview.png";
 
 export const Memoji = () => {
   return (
     <div className={css.container}>
-      <img
-        src="../../../public/Mac-removebg-preview.png"
-        className={css.image}
-      ></img>
-      <img
-        src="../../../public/memoji.png"
-        className={css.memoji}
-        width="339px"
-      ></img>
+      <img src={laptop} className={css.image}></img>
+      <img src={memoji} className={css.memoji} width="339px"></img>
     </div>
   );
 };
